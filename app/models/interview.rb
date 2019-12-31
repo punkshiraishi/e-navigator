239991ae6cc0b05a251e6a02667c226269d342eb
punkshiraishi <pharmacy.sy@gmail.com>
2019-12-31 17:04:44 +0900

    Create relations between users and interviews

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Interview < ApplicationRecord
+  belongs_to :interviewer, class_name: 'User'
+  validates :interviewer_id, presence: true
+  validates :interviewee_id, presence: true
+  validates :schedule, presence: true
 end
