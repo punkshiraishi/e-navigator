80ee71304d16993d1a7d0b744fdf1b3f1a158f6c
punkshiraishi <pharmacy.sy@gmail.com>
2020-01-02 18:25:01 +0900

    Add attendable status display

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -5,4 +5,12 @@ class Interview < ApplicationRecord
   validates :interviewer_id, presence: true
   validates :interviewee_id, presence: true
   validates :schedule, presence: true
+
+  def status
+    case attendable
+    when nil then '保留中'
+    when true then '確定'
+    when false then '出席不可'
+    end
+  end
 end
